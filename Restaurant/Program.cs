053bb7face7a9815b597731fac3afbe49c790b0e
lovernashiga-code <lovernashiga@gmail.com>
2026-01-09 15:51:46 +0700

    Add project files.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,22 @@
+namespace Restaurant
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  Nattawut klubthong 138
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            // Application.Run(new KitchenForm());
+
+            if (args.Length > 0 && args[0].ToLower() == "-k")
+                Application.Run(new KitchenForm());
+            else
+                Application.Run(new WaiterForm());
+        }
+    }
+}
---
+++ Restaurant/WaiterForm.cs
@@ -0,0 +1,20 @@
+namespace Restaurant
+{
+    public partial class WaiterForm : Form
+    {
+        public WaiterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
